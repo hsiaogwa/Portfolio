191dacb2db84bae8675a3664313c0545936a89ca
Alven <hsiaogwa@gmail.com>
2025-10-29 11:32:55 +0800

    Delete app/Index/react/src/classes directory
    delete the dist-dir pushed wrong

--- app/Index/react/src/classes/Ref.js
+++
@@ -1,9 +0,0 @@
-export class Ref {
-    value(data) {
-        if (data) {
-            this.__value.set(data);
-            this.__value.listener();
-        }
-        return this.__value.get();
-    }
-}
--- app/Index/react/src/classes/index.js
+++
@@ -1,5 +0,0 @@
-import { LocalTranslation } from "./LocalTranslation.js";
-import { Ref } from "./Ref.js";
-import { Toggler } from "./Toggler.js";
-import { Typer } from "./Typer.js";
-export { LocalTranslation, Ref, Toggler, Typer };
